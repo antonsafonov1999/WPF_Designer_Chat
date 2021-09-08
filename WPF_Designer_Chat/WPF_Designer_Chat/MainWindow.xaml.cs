72fefff0f9762bf49032ee5799f341808a480e68
Антон Сафонов <antonsafonov1999@gmail.com>
2021-09-08 15:29:49 +0300

    Create Application Resources

--- WPF_Designer_Chat/WPF_Designer_Chat/MainWindow.xaml.cs
+++ WPF_Designer_Chat/WPF_Designer_Chat/MainWindow.xaml.cs
@@ -24,5 +24,30 @@ namespace WPF_Designer_Chat
         {
             InitializeComponent();
         }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove(); 
+        }
+
+        private void ButtonMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow.WindowState = WindowState.Minimized;
+        }
+
+        private void WindowSateButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Application.Current.MainWindow.WindowState != WindowState.Maximized)
+                Application.Current.MainWindow.WindowState = WindowState.Maximized;
+            else
+                Application.Current.MainWindow.WindowState = WindowState.Normal;
+        }
+
+    
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
     }
 }
